00c82ef09bb000e7702af63c0682c09ea41d94ad
Nikita <mr.thekotik@gmail.com>
2022-09-15 11:38:15 +0300

    start the project

---
+++ stock/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from stock.models import Stock,Currency
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "name", "description")
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
